7edb10e41e814a4ba35b1cebb0001f6717ffd457
Mansi2102003 <mthakkar@logilite.com>
2025-09-18 12:29:59 +0530

    Added PrimaryNo.java for prime number check

---
+++ src/Basic/PrimaryNo.java
@@ -0,0 +1,30 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class PrimaryNo {
+	public static boolean isPrime(int num) {
+		if(num<=-1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+		    if (num % i == 0) {
+		        return false;
+		    }
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number which you want to chech whether the number is prime or not: ");
+		int num = sc.nextInt();
+		
+		if(isPrime(num)) {
+			System.out.println(num+ " is a prime number");
+		}else {
+			System.out.println(num+ " is not a prime number");
+		}
+	}
+
+}
