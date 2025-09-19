8d79c89baa490c64ccb9f185b7907b1cc4dd50ff
Mansi Thakkar <mthakkar@logilite.com>
2025-09-19 13:46:22 +0530

    "Added ANT file for demo purpose whether is it working properly or not"

---
+++ src/com/login/DemoTestcases.java
@@ -0,0 +1,14 @@
+package com.login;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DemoTestcases {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/com/login/LoginServiceTest.java
+++ src/com/login/LoginServiceTest.java
@@ -1,9 +1,9 @@
 package com.login;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 class LoginServiceTest {
 	 @Test
 	    public void testValidLogin() {
