23a73f3392e9db29601eed96fc2d563b78915c24
Mansi Thakkar <mthakkar@logilite.com>
2025-09-23 15:07:23 +0530

    "Created method to insert a Node in Stack Linkedlist"

--- src/Basic/StackUsingLinkedList.java
+++ src/Basic/StackUsingLinkedList.java
@@ -4,16 +4,28 @@ class Node{
 	int data;
 	Node ref;
 	
-	Node(int data, int ref){
+	Node(int data){
 		this.data = data;
 		this.ref = null ;
 	}
 }
 
+class StackLinkedList{
+	private  Node top = null;
+	
+	public void push(int val) {
+		Node newNode = new Node(val);
+		newNode.ref = top;
+		top = newNode;
+		System.out.println("Node element sucessfully ");
+	}
+}
 public class StackUsingLinkedList {
 
 	public static void main(String[] args) {
 		
+		StackLinkedList stack = new StackLinkedList();
+		stack.push(11);
 	}
 
 }
