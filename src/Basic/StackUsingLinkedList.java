86192291e9cce5320720a9c12901a2c91a4c0009
Mansi Thakkar <mthakkar@logilite.com>
2025-09-23 14:46:51 +0530

    "Node class is created to Create a node in Stack"

---
+++ src/Basic/StackUsingLinkedList.java
@@ -0,0 +1,19 @@
+package Basic;
+
+class Node{
+	int data;
+	Node ref;
+	
+	Node(int data, int ref){
+		this.data = data;
+		this.ref = null ;
+	}
+}
+
+public class StackUsingLinkedList {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
