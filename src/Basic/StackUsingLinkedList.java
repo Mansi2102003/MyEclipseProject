a1d23a933771405e6744eee4f1d9dbae6026924e
Mansi Thakkar <mthakkar@logilite.com>
2025-09-23 15:58:07 +0530

    "Implemented switch case to perform Consecutive Operation"

--- src/Basic/StackUsingLinkedList.java
+++ src/Basic/StackUsingLinkedList.java
@@ -1,12 +1,15 @@
 package Basic;
 
+import java.util.Scanner;
+
 class Node{
-	int data;
-	Node next;
+	int data;   // Holds the value of the node
+    Node next;  // Points to the next node in the list
 	
-	Node(int data){
+	Node(int data){	// Constructor to create a new node
+
 		this.data = data;
-		this.next = null ;
+		this.next = null ;	// Initially, this node does not point to any other node
 	}
 }
 
@@ -15,9 +18,9 @@ class StackLinkedList{
 	
 	public void push(int val) {
 		Node newNode = new Node(val);
-		newNode.next= top;
+		newNode.next= top;		// Initially, this node does not point to any other node
 		System.out.println(newNode.next);
-		top = newNode;
+		top = newNode;	// Initially, this node does not point to any other node
 		System.out.println(top);
 		System.out.println("Node element sucessfully " +val);
 	}
@@ -27,7 +30,7 @@ class StackLinkedList{
 			System.out.println("Stack is Empty ");
 		}else {
 			System.out.println("Element deleted sucessfully, Deleted element: "+ top.data);
-			top = top.next;
+			top = top.next; 	// Initially, this node does not point to any other node
 		}
 	}
 	
@@ -39,6 +42,7 @@ class StackLinkedList{
 			System.out.println("Stack Elements are: ");
 			while(top!= null) {
 				System.out.println(temp.data + "  ");
+				temp = temp.next; //Move to the next node
 			}
 			System.out.println("Stack is Empty");
 		}
@@ -48,13 +52,39 @@ public class StackUsingLinkedList {
 
 	public static void main(String[] args) {
 		
-		StackLinkedList stack = new StackLinkedList();
-		stack.push(11);
-		stack.pop();
-		stack.push(22);
-		stack.push(33);
-		stack.push(44);
-		stack.pop();
+		StackLinkedList stack = new StackLinkedList(); // Create a new stack
+		Scanner sc = new Scanner(System.in);  // Create a scanner for user input
+		         
+		int choice, value;  // Variables for user choice and input value
+
+		System.out.println(":: Stack using Linked List in Java ::");
+
+		while (true) {  
+			System.out.println("\n.......MENU.......");
+		    System.out.println("1. Push\n2. Pop\n3. Display\n4. Exit");
+		    System.out.print("Enter your choice: ");
+		    choice = sc.nextInt();  
+		            
+		    	switch (choice) {
+		        	case 1:  
+		                    System.out.print("Enter the value to insert: ");
+		                    value = sc.nextInt();  // Get the value to push
+		                    stack.push(value);  
+		                    break;
+		            case 2:  
+		                    stack.pop();  
+		                    break;
+		            case 3:  
+		                    stack.displayStack();  
+		                    break;
+		            case 4:  // User wants to exit the program
+		                    System.out.println("Exiting program...");
+		                    sc.close(); 
+		                    System.exit(0);  // Exit the program
+		            default:  // Invalid choice
+		                    System.out.println("Wrong selection! Please try again.");
+		            }
+		        }
 	}
 
 }
