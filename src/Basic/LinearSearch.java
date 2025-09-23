0de7ff27941cb3e9eaf494de42c7f003d5ac38e4
Mansi Thakkar <mthakkar@logilite.com>
2025-09-23 18:11:38 +0530

    "Implemented Linear Search Algorithm"

--- src/Basic/LinearSearch.java
+++ src/Basic/LinearSearch.java
@@ -1,5 +1,7 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class LinearSearch {
 	public static int linear_Search(int[]arr, int key) {
 		for(int i=0;i<=arr.length-1;i++) {
@@ -11,6 +13,28 @@ public class LinearSearch {
 	}
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of an array: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter array elements: ");
+		for(int i =0; i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the Element which you want to search: ");
+		int key = sc.nextInt();
+		
+		int result = linear_Search(arr, key);
+		
+		if(result!=-1) {
+			System.out.println("Element present at Index: "+result);
+		}else {
+			System.out.println("Element not present in Array");
+		}
 		
 	}
 
